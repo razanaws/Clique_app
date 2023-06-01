7c2e778285b239e013ec029391d88e66e75097de
Razan Aws <razansm99@gmail.com>
2023-06-01 14:24:39 +0300

    Final Product + Class Diagram

--- lib/screens/createProfile/createprofile.dart
+++ lib/screens/createProfile/createprofile.dart
@@ -6,8 +6,6 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:clique/models/user.dart';
 
-import '../navigationBar/NavBar.dart';
-
 
 
 class CreateProfile extends StatefulWidget {
--- lib/screens/profile/MusicianProfile.dart
+++ lib/screens/profile/MusicianProfile.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
+
 class MusicianProfile extends StatefulWidget {
   const MusicianProfile({Key? key}) : super(key: key);
 
